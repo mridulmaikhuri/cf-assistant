0fff28ed8ef3e9e3a042fb561ad256b64cc1d9d2
mridulmaikhuri <mridulmaikhuri1234@gmail.com>
2026-03-18 21:21:49 +0530

    commit

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, HTTPException
 from collections import defaultdict
 from datetime import datetime, timedelta
 from cachetools import TTLCache
+from math import sqrt
 
 BASE_CF_API = "https://codeforces.com/api"
 
@@ -150,8 +151,14 @@ def fetch_problemset_and_tags():
     if "data" not in problemset_cache:
         url = f'{BASE_CF_API}/problemset.problems'
         res = fetch_cf_api(url)
-        problems = res["result"]["problems"]
-        tags = set(tag for prob in problems for tag in prob.get("tags", []))
+        data = res["result"]["problems"]
+        
+        tags = set(tag for prob in data for tag in prob.get("tags", []))
+        problems = defaultdict(list)
+        
+        for prob in data:
+            if "rating" in prob:
+                problems[prob["rating"]].append(prob)
         
         problemset_cache["data"] = {
             "problems": problems,
@@ -196,11 +203,23 @@ def parse_submission(submissions):
 def get_tag_weakness(all_tags, attempted_tags, solved_tags):
     tag_weakness = {}
     
+    total_attempts = sum(attempted_tags.values())
+    
+    # For new users assign equal weakness to all tags
+    if total_attempts == 0:
+        return {tag: 0.5 for tag in all_tags}
+    
+    c = 10
+    k = c / sqrt(total_attempts + 1)
+    p = 0.5
+    
     for tag in all_tags:
         attempted = attempted_tags[tag]
         solved = solved_tags[tag]
-        success_rate = solved / attempted if attempted > 0 else 0
+        
+        success_rate = (solved + k * p) / (attempted + k)
         weakness = 1 - success_rate
+        
         tag_weakness[tag] = weakness
     
     return tag_weakness
@@ -212,48 +231,75 @@ def get_candidates(rating, all_problems, solved_problems, tag_weakness, attempte
     max_rating = ((max_rating + 99) // 100) * 100
     
     candidates = []
-    for prob in all_problems:
-        prob_rating = prob.get("rating")
+    for r in range(min_rating, max_rating + 1, 100):
+        for prob in all_problems.get(r, []):
+            prob_rating = prob.get("rating")
+            
+            if (prob_rating is None):
+                continue
+            
+            if not (min_rating <= prob_rating <= max_rating):
+                continue
+            
+            contest_id = prob.get("contestId")
+            index = prob.get("index")
+            name = prob.get("name")
+            tags = prob.get("tags", [])
+            
+            if (contest_id is None or index is None or not tags):
+                continue
+            
+            problem_key = (contest_id, index)
+            
+            if problem_key in solved_problems:
+                continue
+            
+            weaknesses = [tag_weakness.get(tag, 1) for tag in tags]
+            
+            rating_range = max_rating - min_rating
+            
+            avg_weakness = sum(weaknesses) / len(weaknesses)
+            rms_weakness = sqrt(sum(w*w for w in weaknesses) / len(weaknesses))
+            unseen_bonus = 1 if problem_key not in attempted_problems else 0
+            rating_bonus = (1 - abs(prob_rating - rating) / rating_range)
+            tag_score = min(len(tags), 3) / 3
+            
+            # parameters which are used for score generation are as follows
+            # 1. average weakness: to get average weakness of all tags in the problems and also to make sure that we get balanced training
+            # 2. rms weakness: to make sure that very weak tags get slight nudge in the scoring
+            # 3. len(tags): to make sure that more the no of tags the more weightage it gets basically to make sure that more diverse problems get more weightage
+            # 4. unseen_bonus: to give bonus to unseen problems it ensures that never seen problems are recommended first
+            # 5. rating_bonus: to make sure that problems closer to the rating ranger are preferred first
+            score = 0.8 * avg_weakness + 0.2 * rms_weakness + 0.1 * tag_score + 0.1 * unseen_bonus + 0.1 * rating_bonus
+            
+            candidates.append({
+                "contestId": contest_id,
+                "index": index,
+                "name": name,
+                "rating": prob_rating,
+                "tags": tags,
+                "score": round(score, 4),
+                "link": f"https://codeforces.com/problemset/problem/{contest_id}/{index}"
+            })
+    
+    candidates.sort(key=lambda x : (-x["score"], abs(x["rating"] - rating)))
+    
+    # Ensures diverse tags in recommendations
+    # This makes sure that only problems of one tag don't get recommended
+    tag_counter = defaultdict(int)
+
+    for candidate in candidates:
+        penalty = 0
         
-        if (prob_rating is None):
-            continue
+        for tag in candidate["tags"]:
+            penalty += (tag_counter[tag] ** 1.3) * 0.01 
         
-        if not (min_rating <= prob_rating <= max_rating):
-            continue
+        candidate["score"] -= penalty
         
-        contest_id = prob.get("contestId")
-        index = prob.get("index")
-        name = prob.get("name")
-        tags = prob.get("tags", [])
+        diverse_candidates.append(candidate)
         
-        if (contest_id is None or index is None):
-            continue
-        
-        problem_key = (contest_id, index)
-        
-        if problem_key in solved_problems:
-            continue
-        
-        if not tags:
-            continue
-        
-        avg_weakness = sum(tag_weakness.get(tag, 1) for tag in tags) / len(tags)
-        unseen_bonus = 0.15 if problem_key not in attempted_problems else 0
-        
-        rating_bonus = 1 - abs(prob_rating - rating) / 200
-        rating_bonus *= 0.1
-        
-        score = avg_weakness + unseen_bonus + rating_bonus
-        
-        candidates.append({
-            "contestId": contest_id,
-            "index": index,
-            "name": name,
-            "rating": prob_rating,
-            "tags": tags,
-            "score": round(score, 4),
-            "link": f"https://codeforces.com/problemset/problem/{contest_id}/{index}"
-        })
+        for tag in candidate["tags"]:
+            tag_counter[tag] += 1
     
     candidates.sort(key=lambda x : (-x["score"], abs(x["rating"] - rating)))
     
